a481ec13cacf465f7daf71931e16d366e750d09c
Emmanuel O <cciskenya@gmail.com>
2022-01-24 12:55:18 +0300

    threat detector

--- threat_detector.py
+++ threat_detector.py
@@ -2,35 +2,47 @@
 
 def palindrome_finder(message):
     palindromes = []
-    
+
     for i in range(len(message)):
-            for j in range(i+1, len(message) + 1):
-                temp = message[i:j]
-                if temp == temp[::-1] and len(temp) >= 3: # the valid palindrome has 3 or more chars
-                    palindromes.append(temp)
+        for j in range(i+1, len(message) + 1):
+            temp = message[i:j]
+            # the valid palindrome has 3 or more chars
+            if temp == temp[::-1] and len(temp) >= 3:
+                palindromes.append(temp)
     return palindromes
 
+
 def threatDetector(textMessages):
-    symbols = [ message[len(message)-3:] for message in textMessages] # extract symbols from our message
-    new_message = [message[:-3] for message in textMessages] # message after removing symbol
-    palindromes = [ palindrome_finder(message) for message in new_message ] # get all possible palindromes in new message
-    palindrome_sublists_size = [len(x) for sublist in palindromes for x in sublist] # get size of each individual palindrome substring
-    palindrome_array_size = [len(x) for x in palindromes] # get size of each message's palindrome array
-    palindromes_size = len(palindromes) # overrall size of our palindromes array
+    # extract symbols from our message
+    symbols = [message[len(message)-3:] for message in textMessages]
+    new_message = [message[:-3]
+                   for message in textMessages]  # message after removing symbol
+    # get all possible palindromes in new message
+    palindromes = [palindrome_finder(message) for message in new_message]
+    # get size of each individual palindrome substring
+    palindrome_sublists_size = [len(x)
+                                for sublist in palindromes for x in sublist]
+    # get size of each message's palindrome array
+    palindrome_array_size = [len(x) for x in palindromes]
+    # overrall size of our palindromes array
+    palindromes_size = len(palindromes)
     scores = []
     risk_score = []
     result = []
 
     if palindromes_size > 0:
         for array_size in palindrome_array_size:
-            new_palindrome_sublists_size = palindrome_sublists_size[:array_size] # get score of first n palindromes
-            del palindrome_sublists_size[:array_size] # update `palindrome_sublists_size` by removing from original sublist size already calculated scores
-            scores.append(sum(new_palindrome_sublists_size)) # get total score for each message
-        
-        palindromes_size -= 1 # reduce palindromes_size by 1 after each message score calculation signifying calculation of each message's score
+            # get score of first n palindromes
+            new_palindrome_sublists_size = palindrome_sublists_size[:array_size]
+            # update `palindrome_sublists_size` by removing from original sublist size already calculated scores
+            del palindrome_sublists_size[:array_size]
+            # get total score for each message
+            scores.append(sum(new_palindrome_sublists_size))
+
+        palindromes_size -= 1  # reduce palindromes_size by 1 after each message score calculation
 
     # score matching
-    if len(palindrome_array_size) >= 2: # detect threat when there are 2 or more palindromes
+    if len(palindrome_array_size) >= 2:  # detect threat when there are 2 or more palindromes
         for score in scores:
             if score in range(1, 11):
                 risk_score.append("Possible")
@@ -40,11 +52,12 @@ def threatDetector(textMessages):
                 risk_score.append("Escalate")
             else:
                 risk_score.append("Ignore")
-    
+
     for i in range(len(symbols)):
-        result.append(symbols[i] + " " + risk_score[i]) # concatenate each  symbol with its score
-    
-    return "\n".join(result) # return the result on new line
+        # concatenate each  symbol with its score
+        result.append(symbols[i] + " " + risk_score[i])
+
+    return "\n".join(result)  # return the result on new line
 
 
 textMessages = ['xxxayyySPY', 'xxxxxxbzzzzzzIVV', 'xxAyyDJI']
